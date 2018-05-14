2e523d4b0068d29c520deb164715f07dba85eae4
Mike <al-mn@ya.ru>
2018-05-14 12:31:18 +0300

    initial

---
+++ Intermediate algorithms/wherefore-art-thou.js
@@ -0,0 +1,79 @@
+// Wherefore art thou
+
+// Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching property and value pairs (second argument). Each property and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
+
+
+function whatIsInAName(collection, source) {
+  // What's in a name?
+  var arr = [];
+  var sourceKeys = Object.keys(source);
+  
+  return collection.filter(function(obj){
+    
+    for(var i = 0; i < sourceKeys.length; i++) {
+      
+      if(!obj.hasOwnProperty(sourceKeys[i]) || obj[sourceKeys[i]] !== source[sourceKeys[i]]) {
+        return false;
+      }  
+      
+    }
+    
+    return true;
+    
+  });
+  
+
+}
+
+whatIsInAName([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "b": 2 });
+
+
+
+// Intermediate Code Solution:
+
+function whatIsInAName(collection, source) {
+  // "What's in a name? that which we call a rose
+  // By any other name would smell as sweet.”
+  // -- by William Shakespeare, Romeo and Juliet
+  var srcKeys = Object.keys(source);
+  // We filter through the collection using .filter().
+  return collection.filter(function (obj) {
+    // Next, we return a Boolean value for the .filter() method.
+    // Finally, we reduce to Boolean value to be returned for the .every() method.
+    return srcKeys.every(function (key) {
+      return obj.hasOwnProperty(key) && obj[key] === source[key];
+    });
+  });
+}
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+
+
+
+
+// Advanced Code Solution:
+
+function whatIsInAName(collection, source) {
+
+  var srcKeys = Object.keys(source);
+
+  // filter the collection
+  return collection.filter(function (obj) {
+    return srcKeys
+      .map(function(key) {
+        return obj.hasOwnProperty(key) && obj[key] === source[key];
+      })
+      .reduce(function(a, b) {
+        return a && b;
+      });
+  });
+}
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+
+
+// Code Explanation:
+// We start by filtering through collection using Array.filter().
+// Next, we map through all keys and return Boolean values based on the check conditions: both the key and its corresponding value must exist within the object we are filtering through.
+// Then we reduce the mapped Boolean values to a single Boolean that indicates whether all srcKeys pass the conditions checked above.
+// This single Boolean will be used to filter through the collection.
